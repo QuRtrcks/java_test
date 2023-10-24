172756a3e7550878f463eaacb87e09a722f674ff
Egor Zame <egor.zam44@gmail.com>
2023-10-24 16:46:54 +0300

    добавлено несколько функций

--- qaa-courses-java/src/main/java/ru/stqa/pft/sandbox/main1.java
+++ qaa-courses-java/src/main/java/ru/stqa/pft/sandbox/main1.java
@@ -3,5 +3,17 @@ package ru.stqa.pft.sandbox;
 public class main1{
     public static void main(String[] args){
         System.out.println("Hello, world!");
+        System.out.println("Hello"+","+"World");
+        int s=89;
+        int l= s*s;
+        System.out.println("Плащадь квадрата со стороной-"+s+" = "+l);
+        System.out.println("При известной площади квадрата "+l+",мы можем узнать стоймость материалов");
+
     }
+
+    public static void  hello() {
+        System.out.println("Hello, world!");
+    }
+
 }
+
---
+++ qaa-courses-java/src/main/java/ru/stqa/pft/sandbox/test.java
@@ -0,0 +1,30 @@
+package ru.stqa.pft.sandbox;
+
+public class test {
+  public static void main(String[] args){
+  hello("world");
+  hello("Egor");
+
+
+  double l=2;
+  System.out.println("Площадь квадрата со сторонами "+l+" ="+area(l));
+
+
+  double a=4;
+  double b=6;
+    System.out.println("Площадь прямоугольника со сторонами "+a+" и "+b+" = "+area(a,b));
+  }
+
+
+  public static void hello(String somebody){
+    System.out.println("Hello, "+somebody);
+
+  }
+  public static double area(double l){
+    return l*l;
+  }
+  public static double area(double a, double b){
+    return a*b;
+  }
+}
+
